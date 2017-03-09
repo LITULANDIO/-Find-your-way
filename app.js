78bdebf7390fd4d2ab930cb7b5005df732a7e3da
Litus <litulandio@gmail.com>
2017-03-09 15:19:19 +0100

    edit app.js

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public/html'))
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/html/index.html')
-})
+// app.get('/', function (req, res) {
+//   res.sendFile(__dirname + '/public/html/index.html')
+// })
 
 // app.listen(PORT, function () {
 //   console.log('Runing...')
