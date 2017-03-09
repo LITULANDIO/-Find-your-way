fe8bef80736e43a2bba65dd25a3e5552f6313c9d
Litus <litulandio@gmail.com>
2017-03-09 15:50:31 +0100

    edit app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-app.use(express.static(__dirname + '/public/html'))
+app.use(express.static(__dirname + '/public/'))
 
 // app.get('/', function (req, res) {
 //   res.sendFile(__dirname + '/public/html/index.html')
