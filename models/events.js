d12245a94ffa9393a6fffff02cf817cbfa38dbbf
Litus <litulandio@gmail.com>
2017-03-14 14:31:19 +0100

    pug

--- models/events.js
+++ models/events.js
@@ -5,10 +5,16 @@ const EventSchema = new mongoose.Schema({
   // owner: { type: String, required: true },
   category: { type: String, required: true },
   title: { type: String, default: false },
+  path: String,
+  comunity: String,
   localitation: String,
   createdEvent: { type: Number, default: Date.now },
   startDate: String,
+  timeHourStart: Number,
+  timeMinuteStart: Number,
   endDate: String,
+  timeHourEnd: Number,
+  timeMinuteEnd: Number,
   description: String,
   tags: String
 }, { collection })
--- routes/clients/index.js
+++ routes/clients/index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const getAll = require('./handlers/getAll')
+const addEvents = require('./handlers/addEvents')
 
 router.get('/', getAll) // localhost:3010/clients
+router.post('/', addEvents)
 
 module.exports = router
+
