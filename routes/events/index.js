996ee1c7f649edd3a38093bdb3a97e7eddf21326
Litus <litulandio@gmail.com>
2017-03-20 03:04:08 +0100

    design html event-detail

--- routes/events/index.js
+++ routes/events/index.js
@@ -7,13 +7,15 @@ const getRetiros = require('./handlers/getRetiros')
 const showForm = require('./handlers/showForm')
 const addRetiros = require('./handlers/addRetiros')
 const myAcount = require(('./handlers/getCategories'))
+
 // const showAcount = require('./handlers/ShowAcount')
 
 router.get('/', Home) // 1 ruta muestra home
-router.get('/retiros', getRetiros)
-router.get('/acount', myAcount)
-router.get('/retiros/add', showForm)
-router.post('/retiros', addRetiros)
+router.get('/retiros', getRetiros) // Muestra todos los retiros
+router.get('/acount', myAcount) // Entramos en la cuenta personal
+router.get('/retiros/add', showForm) // Vemos el formulario vacio
+router.post('/retiros', addRetiros) // Guardamos los datos del formulario
+
 // router.get('/acount', showAcount) // entrar en la zona privada
 // router.get('/', getAl) // localhost:3010/clients
 // router.post('/add', addRetiros)
