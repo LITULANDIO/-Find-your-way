50b2d0f9f8c1d92a59efd7aceb35fe297b58664e
Litus <litulandio@gmail.com>
2017-03-23 21:50:58 +0100

    view maps dinamic :) ueeee!!!

--- public/js/mapsEvent.js
+++ public/js/mapsEvent.js
@@ -1,9 +1,21 @@
 
 function initMap () {
+  var lat = document.getElementById('lat').value
+  var long = document.getElementById('long').value
+  var parseLat = parseFloat(lat)
+  var parseLong = parseFloat(long)
+
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 5,
-    center: retiro.localitation
+
+    zoom: 12,
+    center: {lat: parseLat, lng: parseLong}
   })
+
+  var marker = new google.maps.Marker({
+    map: resultsMap,
+    position: results[0].geometry.location
+  })
+
   var geocoder = new google.maps.Geocoder()
   var infowindow = new google.maps.InfoWindow()
 }
