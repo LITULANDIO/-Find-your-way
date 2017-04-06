5ab37a0dbe42545442990459497fc46f3c796722
Litus <litulandio@gmail.com>
2017-04-07 01:12:10 +0200

    insert maps in search events & tantra

--- public/js/insert-title.js
+++ public/js/insert-title.js
@@ -1,11 +1,10 @@
- $(document).ready(function (e) {
-   e.preventDefault()
-   if ($(location).attr('href', '/retiros/Yoga')) {
-     $('#title-mine').append('Retiros de Yoga')
-   } else if ($(location).attr('href', '/retiros/Ayurveda')) {
-     $('#title-mine').append('Retiros de Ayurveda')
-   } else if ($(location).attr('href', '/retiros')) {
-     $('#title-mine').append('Retiros de Yoga y Ayurveda')
+ $(document).ready(function () {
+   if ('/retiros/Yoga') {
+     $('#title-mine').text('Retiros de Yoga')
+   } else if ('/retiros/Ayurveda') {
+     $('#title-mine').text('Retiros de Ayurveda')
+   } else if ('/retiros') {
+     $('#title-mine').text('Retiros de Yoga y Ayurveda')
    }
  })
 
