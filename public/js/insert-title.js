ad5c4fb1b48c4862ed797c0fe0d999666a14a3b5
Litus <litulandio@gmail.com>
2017-04-07 15:17:52 +0200

    finsih title dynamic

--- public/js/insert-title.js
+++ public/js/insert-title.js
@@ -1,10 +1,9 @@
- $(document).ready(function () {
-   if ('/retiros/Yoga') {
-     $('#title-mine').text('Retiros de Yoga')
-   } else if ('/retiros/Ayurveda') {
-     $('#title-mine').text('Retiros de Ayurveda')
-   } else if ('/retiros') {
-     $('#title-mine').text('Retiros de Yoga y Ayurveda')
-   }
- })
-
+$(document).ready(function () {
+  if (window.location.pathname == '/retiros/Yoga') {
+    $('#title-mine').text('Retiros de Yoga')
+  } else if (window.location.pathname == '/retiros/Ayurveda') {
+    $('#title-mine').text('Retiros de Ayurveda')
+  } else if (window.location.pathname == '/retiros') {
+    $('#title-mine').text('Retiros de Yoga y Ayurveda')
+  }
+})
