7135f7a7fb5f496bc111a4609597407d14de6b7a
Litus <litulandio@gmail.com>
2017-04-15 02:45:03 +0200

    structuring maps

--- routes/events/handlers/addRetiros.js
+++ routes/events/handlers/addRetiros.js
@@ -14,7 +14,7 @@ module.exports = (req, res) => {
   console.log('He recibido lo siguiente', req.body)
   const { title } = req.body
   const { category } = req.body
-  const { imageUrl } = req.file
+  // const { imageUrl } = req.file
   const { road } = req.body
   const { lat } = req.body
   const { long } = req.body
@@ -28,14 +28,14 @@ module.exports = (req, res) => {
   const { timeMinuteEnd } = req.body
   const { description } = req.body
   const { tags } = req.body
-  const retiro = new Retiro({ title, category, imageUrl, road, comunity, localitation, lat, long, startDate, timeHourStart, timeMinuteStart, endDate, timeHourEnd, timeMinuteEnd, description, tags })
+  const retiro = new Retiro({ title, category, road, comunity, localitation, lat, long, startDate, timeHourStart, timeMinuteStart, endDate, timeHourEnd, timeMinuteEnd, description, tags })
 
-  if (req.file) {
-    cloudinary.uploader.upload(req.file.path,
-  function (result) {
-    retiro.imageUrl = result.url
-  })
-  }
+  // if (req.file) {
+  //   cloudinary.uploader.upload(req.file.path,
+  // function (result) {
+  //   retiro.imageUrl = result.url
+  // })
+  // }
 
   retiro.save()
   console.log('Retiro creado' + retiro)
