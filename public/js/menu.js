7b25cb6f587748a0bb97ac0c83aab10e18b3b019
Litus <litulandio@gmail.com>
2017-08-21 03:09:55 +0200

    hover

---
+++ public/js/menu.js
@@ -0,0 +1,23 @@
+
+// menu: hover Encuentra
+
+  $(document).ready(function () {
+    $('.menu_li').on({
+      moouseover: function () {
+        $(this).addClass('hover_menu')
+        $(this).find('ul').fadeIn(300)
+        $('.menu_li ul').on({
+          mouseover: function () {
+            $(this).parent('.menu_li').addClass('hover_menu')
+          },
+          mouseleave: function () {
+            $(this).parent().find('.menu_lis').removeClass('hover_menu')
+          }
+        })
+      },
+      mouseleave: function () {
+        $(this).find('ul').fadeOut(300)
+        $(this).removeClass('hover_menu')
+      }
+    })
+  })
